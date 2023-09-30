3a19cbd90c271c5111daa168830f488b7f5389b4
saberqadimi <qadimi43@gmail.com>
2023-09-30 16:16:10 +0330

    refactor package trait file

--- src/Searchable.php
+++ src/Searchable.php
@@ -15,23 +15,18 @@ trait Searchable
      */
     public function search($query, string $searchText, array $searchColumns, array $relationColumns = []): Builder
     {
-        return $query->where(function($query) use ($searchText, $searchColumns, $relationColumns) {
-
+        return $query->where(function ($query) use ($searchText, $searchColumns, $relationColumns) {
             foreach ($searchColumns as $searchColumn) {
-                $query->orWhere($searchColumn, 'like', '%' .$searchText. '%');
+                $query->orWhere($searchColumn, 'like', '%' . $searchText . '%');
             }
 
             foreach ($relationColumns as $key => $relationColumn) {
-                $query->orWhere(function($query) use($relationColumn, $key, $searchText) {
-                    return $query->whereHas($key, function ($query) use($relationColumn, $searchText) {
-                        foreach ($relationColumn as $column) {
-                            $query->where($column, 'like', '%' .$searchText. '%');
-                        }
-                    });
+                $query->orWhereHas($key, function ($query) use ($relationColumn, $searchText) {
+                    foreach ($relationColumn as $column) {
+                        $query->where($column, 'like', '%' . $searchText . '%');
+                    }
                 });
             }
-
-            return $query;
         });
     }
 
